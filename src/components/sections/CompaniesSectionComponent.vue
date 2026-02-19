2bce25f095ac5e1f121f1db2901c56accd5f4fbf
edoriggio <edo.riggio19@gmail.com>
2026-02-19 16:35:20 +0100

    added main sponsor + some small modifications

--- src/components/sections/CompaniesSectionComponent.vue
+++ src/components/sections/CompaniesSectionComponent.vue
@@ -3,27 +3,28 @@
     <span class="text-3xl text-center">Our Sponsors</span>
 
     <span class="flex flex-col items-center gap-20 justify-center max-w-[990px]">
-      <span
-          v-for="category in categories()"
-          :key="category.valueOf()"
-          class="flex flex-col items-center justify-center gap-5"
-          :class="category === 'project' ? 'max-w-[500px]' : 'max-w-[990px]'"
-      >
-        <span class="text-center opacity-60 max-w-[520px]">
-          {{ category[0].toUpperCase()+category.slice(1) }}
-          {{ ['main', 'silver', 'bronze'].includes(category) ? 'Sponsors' : 'Partners' }}
+      <template v-for="category in categories()" :key="category.valueOf()">
+        <span
+            v-if="sponsors.filter(el => el.type.includes(category)).length != 0"
+            class="flex flex-col items-center justify-center gap-5"
+            :class="category === 'project' ? 'max-w-[500px]' : 'max-w-[990px]'"
+        >
+            <span class="text-center opacity-60 max-w-[520px]">
+              {{ category[0].toUpperCase()+category.slice(1) }}
+              {{ ['main', 'gold', 'silver', 'bronze'].includes(category) ? 'Sponsors' : 'Partners' }}
+            </span>
+    
+            <span class="flex items-center justify-center gap-10 flex-wrap" :class="category">
+                <a v-for="sponsor in sponsors.filter(el => el.type.includes(category))" :key="sponsor.name" :href="sponsor.url" target="_blank">
+                    <img
+                        :src="getImage(sponsor.name)"
+                        :alt="`${sponsor.name} logo`"
+                        :class="sponsor.name === 'usi' ? 'h-[70px]' : ['main', 'silver'].includes(category) ?'h-[45px]' : 'h-[30px]'"
+                    />
+                </a>
+            </span>
         </span>
-
-        <span class="flex items-center justify-center gap-10 flex-wrap" :class="category">
-          <a v-for="sponsor in sponsors.filter(el => el.type.includes(category))" :key="sponsor.name" :href="sponsor.url" target="_blank">
-            <img
-                :src="getImage(sponsor.name)"
-                :alt="`${sponsor.name} logo`"
-                :class="sponsor.name === 'usi' ? 'h-[70px]' : ['main', 'silver'].includes(category) ?'h-[45px]' : 'h-[30px]'"
-            />
-          </a>
-        </span>
-      </span>
+      </template>
     </span>
 <!--    <span class="text-center opacity-60">Our Partners</span>-->
 
